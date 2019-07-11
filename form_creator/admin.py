86b060adda94885467035590f732a693b339d20f
Yarden Katz <yardenkmusic@gmail.com>
2019-07-11 23:53:03 +0300

    new form_list
    playground with templates

--- form_creator/admin.py
+++ form_creator/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import UserForm, FormField #,FormList
 
+class UserformAdmin(admin.ModelAdmin):
+	readonly_fields = ('id',)
+
+
+class FormfieldAdmin(admin.ModelAdmin):
+	readonly_fields = ('id',)
+
 # admin.site.register(FormList)
-admin.site.register(UserForm)
-admin.site.register(FormField)
+admin.site.register(UserForm, UserformAdmin)
+admin.site.register(FormField, FormfieldAdmin)
--- form_creator/urls.py
+++ form_creator/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='form_create'),
+	# path('', views.index, name='form_create'),
 	path('<int:form_id>', views.index, name='index'),
-	path('form_list', views.FormListView, name='form_list'),
+	path('', views.FormListView.as_view(), name='form_list'),
 	path('create', views.UserFormCreate.as_view(), name='form_create'),
 	path('form_confirm', views.formSubmitConfirm, name='form_submitted')
 ]
